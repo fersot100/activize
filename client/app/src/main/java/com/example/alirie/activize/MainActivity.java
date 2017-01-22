fb7375efd593e7c8c875bf2b4ab448814bdcbd88
Juan <fersot100@yahoo.com>
2017-01-21 23:27:04 -0800

    Passing the problems to you dear

--- client/app/src/main/java/com/example/alirie/activize/MainActivity.java
+++ client/app/src/main/java/com/example/alirie/activize/MainActivity.java
@@ -4,24 +4,40 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.io.IOException;
-import okhttp3.OkHttpClient;
 
 
 public class MainActivity extends AppCompatActivity {
-    private OkHttpClient client = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(android.R.layout.activity_main);
         //load splash page
         //get list of events
         //populate UI with event cards
         //hide splash, display events
         //toolbar w/ "sort by" and "create event"
         //clicking on an event brings up event page with map
+        String events[] = {
+                "memes",
+                "dreams",
+                "creams",
+                "queens",
+                "peens"
+        };
+        ArrayAdapter<String> eventAdapter =
+                new ArrayAdapter<String>(this,
+                        android.R.layout.event_card,
+                        android.R.id.event,
+                        events
+                );
+        ListView eventList = new ListView(this);
+        setContentView(eventList);
+        eventList.setAdapter(eventAdapter);
     }
 
 
@@ -33,4 +49,5 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(getApplicationContext(), CreateEvent.class);
         startActivity(i);
     }
+
 }
