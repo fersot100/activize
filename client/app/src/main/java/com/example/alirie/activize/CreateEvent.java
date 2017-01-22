24d3ecdac3aa70d93395b10b1183452ce49d94eb
Alirie <gray@oxy.edu>
2017-01-21 16:25:06 -0800

    clean version

--- client/app/src/main/java/com/example/alirie/activize/CreateEvent.java
+++ client/app/src/main/java/com/example/alirie/activize/CreateEvent.java
@@ -44,7 +44,18 @@ public class CreateEvent extends AppCompatActivity {
     Time now = new Time();
     Time eventTime = new Time();
 
-/*
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event);
+        dateElement = (EditText) findViewById(R.id.input_date);
+        timeElement = (EditText) findViewById(R.id.input_time);
+        now.setToNow();
+        year = now.year;
+        month = now.month;
+        day = now.monthDay;
+    }
+
     private class LocatorAsyncTask extends AsyncTask<LocatorFindParameters, Void, List<LocatorGeocodeResult>> {
         private Exception exception;
         @Override
@@ -60,89 +71,62 @@ public class CreateEvent extends AppCompatActivity {
             return results;
         }
     }
-    */
-
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_event);
-
-        dateElement = (EditText) findViewById(R.id.input_date);
-        timeElement = (EditText) findViewById(R.id.input_time);
-        now.setToNow();
-        year = now.year;
-        month = now.month;
-        day = now.monthDay;
-
-//        mapView = (MapView) findViewById(R.id.map);
-//        locationLayer = new GraphicsLayer();
-//        mapView.addLayer(locationLayer);
-//        mapView.setOnStatusChangedListener(new OnStatusChangedListener() {
-//            public void onStatusChanged(Object source, STATUS status) {
-//                if ((source == mapView) && (status == STATUS.INITIALIZED)) {
-//                    isMapLoaded = true;
-//                }
-//            }
-//        });
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        View searchRef = menu.findItem(R.id.action_search).getActionView();
+        searchBar = (EditText) searchRef.findViewById(R.id.searchText);
+        searchBar.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
+                if(keyCode == KeyEvent.KEYCODE_ENTER){
+                    onSearchButtonClicked(searchBar);
+                    return true;
+                }
+                return false;
+            }
+        });
+        return true;
     }
-//
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu){
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        View searchRef = menu.findItem(R.id.action_search).getActionView();
-//        searchBar = (EditText) searchRef.findViewById(R.id.searchText);
-//        searchBar.setOnKeyListener(new View.OnKeyListener() {
-//            @Override
-//            public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
-//                if(keyCode == KeyEvent.KEYCODE_ENTER){
-//                    onSearchButtonClicked(searchBar);
-//                    return true;
-//                }
-//                return false;
-//            }
-//        });
-//        return true;
-//    }
-//
-//    private void locate(String address){
-//        // Create Locator parameters from single line address string
-//        LocatorFindParameters findParams = new LocatorFindParameters(address);
-//
-//        // Use the centre of the current map extent as the find location point
-//        findParams.setLocation(mapView.getCenter(), mapView.getSpatialReference());
-//
-//        // Calculate distance for find operation
-//        Envelope mapExtent = new Envelope();
-//        mapView.getExtent().queryEnvelope(mapExtent);
-//        // assume map is in metres, other units wont work, double current envelope
-//        double distance = (mapExtent != null && mapExtent.getWidth() > 0) ? mapExtent.getWidth() * 2 : 10000;
-//        findParams.setDistance(distance);
-//        findParams.setMaxLocations(2);
-//
-//        // Set address spatial reference to match map
-//        findParams.setOutSR(mapView.getSpatialReference());
-//
-//        // Execute async task to find the address
-//        new LocatorAsyncTask().execute(findParams);
-//        locationLayerPointString = address;
-//    }
-//
-//    public boolean onOptionsItemSelected(MenuItem item){
-//        int id = item.getItemId();
-//        if (id == R.id.action_search) {
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
-//
-//    public void onSearchButtonClicked(View view){
-//        InputMethodManager inputManager = (InputMethodManager) getSystemService(getApplicationContext().INPUT_METHOD_SERVICE);
-//        inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
-//        String address = searchBar.getText().toString();
-//        locate(address);
-//    }
-//
+
+    private void locate(String address){
+        // Create Locator parameters from single line address string
+        LocatorFindParameters findParams = new LocatorFindParameters(address);
+
+        // Use the centre of the current map extent as the find location point
+        findParams.setLocation(mapView.getCenter(), mapView.getSpatialReference());
+
+        // Calculate distance for find operation
+        Envelope mapExtent = new Envelope();
+        mapView.getExtent().queryEnvelope(mapExtent);
+        // assume map is in metres, other units wont work, double current envelope
+        double distance = (mapExtent != null && mapExtent.getWidth() > 0) ? mapExtent.getWidth() * 2 : 10000;
+        findParams.setDistance(distance);
+        findParams.setMaxLocations(2);
+
+        // Set address spatial reference to match map
+        findParams.setOutSR(mapView.getSpatialReference());
+
+        // Execute async task to find the address
+        new LocatorAsyncTask().execute(findParams);
+        locationLayerPointString = address;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if (id == R.id.action_search) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onSearchButtonClicked(View view){
+        InputMethodManager inputManager = (InputMethodManager) getSystemService(getApplicationContext().INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        String address = searchBar.getText().toString();
+        locate(address);
+    }
 
     //opens date picker dialog
     public void pickDate (View v) {
@@ -201,6 +185,6 @@ public class CreateEvent extends AppCompatActivity {
         ;
 
     }
-    //opens place picker
+
 
 }
